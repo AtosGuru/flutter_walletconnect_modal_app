2e7f4ddc705172ad5feecf5001090402efc50c1c
AtosGuru <atosguru7@gmail.com>
2022-08-03 22:52:14 +0300

    project commit

---
+++ flutter_walletconnect_modal_app/lib/algorand_connector.dart
@@ -0,0 +1,127 @@
+import 'package:algorand_dart/algorand_dart.dart';
+import 'package:flutter/material.dart';
+import 'package:walletconnect_dart/walletconnect_dart.dart';
+import 'package:walletconnect_qrcode_modal_dart/walletconnect_qrcode_modal_dart.dart';
+
+import 'wallet_connector.dart';
+
+class AlgorandConnector implements WalletConnector {
+  AlgorandConnector() {
+    _connector = WalletConnectQrCodeModal(
+      connector: WalletConnect(
+        bridge: 'https://bridge.walletconnect.org',
+        clientMeta: const PeerMeta(
+          // <-- Meta data of your app appearing in the wallet when connecting
+          name: 'QRCodeModalExampleApp',
+          description: 'WalletConnect Developer App',
+          url: 'https://walletconnect.org',
+          icons: [
+            'https://gblobscdn.gitbook.com/spaces%2F-LJJeCjcLrr53DcT1Ml7%2Favatar.png?alt=media'
+          ],
+        ),
+      ),
+    );
+
+    _provider = AlgorandWalletConnectProvider(_connector.connector);
+  }
+
+  @override
+  Future<SessionStatus?> connect(BuildContext context) async {
+    return await _connector.connect(context, chainId: 4160);
+  }
+
+  @override
+  void registerListeners(
+    OnConnectRequest? onConnect,
+    OnSessionUpdate? onSessionUpdate,
+    OnDisconnect? onDisconnect,
+  ) =>
+      _connector.registerListeners(
+        onConnect: onConnect,
+        onSessionUpdate: onSessionUpdate,
+        onDisconnect: onDisconnect,
+      );
+
+  @override
+  Future<String?> sendAmount({
+    required String recipientAddress,
+    required double amount,
+  }) async {
+    final sender = Address.fromAlgorandAddress(
+        address: _connector.connector.session.accounts[0]);
+    final recipient = Address.fromAlgorandAddress(address: recipientAddress);
+
+    // Fetch the suggested transaction params
+    final params = await _algorand.getSuggestedTransactionParams();
+
+    // Build the transaction
+    final transaction = await (PaymentTransactionBuilder()
+          ..sender = sender
+          ..noteText = 'Signed with WalletConnectQrCodeModal'
+          ..amount = Algo.toMicroAlgos(amount / 1000000)
+          ..receiver = recipient
+          ..suggestedParams = params)
+        .build();
+
+    // Sign the transaction
+    final txBytes = Encoder.encodeMessagePack(transaction.toMessagePack());
+    final signedBytes = await _provider.signTransaction(
+      txBytes,
+      params: {
+        'message': 'Optional description message',
+      },
+    );
+
+    // Broadcast the transaction
+    try {
+      final txId = await _algorand.sendRawTransactions(
+        signedBytes,
+        waitForConfirmation: true,
+      );
+      return txId;
+    } catch (e) {
+      print('Error: $e');
+    }
+
+    // Kill the session
+    _connector.killSession();
+
+    return null;
+  }
+
+  @override
+  Future<void> openWalletApp() async => await _connector.openWalletApp();
+
+  @override
+  Future<double> getBalance() async {
+    final address = _connector.connector.session.accounts[0];
+    // balance in microAlgos
+    final balance = await _algorand.getBalance(address);
+    return balance / 1000000;
+  }
+
+  @override
+  bool validateAddress({required String address}) {
+    try {
+      Address.fromAlgorandAddress(address: address);
+      return true;
+    } catch (_) {
+      return false;
+    }
+  }
+
+  @override
+  String get faucetUrl => 'https://bank.testnet.algorand.network/';
+
+  @override
+  String get address => _connector.connector.session.accounts[0];
+
+  @override
+  String get coinName => 'Algo';
+
+  late final WalletConnectQrCodeModal _connector;
+  late final AlgorandWalletConnectProvider _provider;
+  late final _algorand = Algorand(
+    algodClient: AlgodClient(apiUrl: AlgoExplorer.TESTNET_ALGOD_API_URL),
+  );
+}
---
+++ flutter_walletconnect_modal_app/lib/main.dart
@@ -0,0 +1,201 @@
+import 'package:flutter/material.dart';
+
+import 'wallet_connector.dart';
+import 'wallet.dart';
+import 'algorand_connector.dart';
+import 'ethereum_connector.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+enum ConnectionState {
+  disconnected,
+  connecting,
+  connected,
+  connectionFailed,
+  connectionCancelled,
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  WalletConnector connector = EthereumConnector();
+
+  static const _networks = ['Ethereum', 'Algorand (Testnet)'];
+
+  ConnectionState _state = ConnectionState.disconnected;
+  String? _networkName = _networks.first;
+
+  @override
+  void initState() {
+    connector.registerListeners(
+        // connected
+        (session) => print('Connected: $session'),
+        // session updated
+        (response) => print('Session updated: $response'),
+        // disconnected
+        () {
+      setState(() => _state = ConnectionState.disconnected);
+      print('Disconnected');
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Wallet Connection Test'),
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(left: 8.0),
+                child: Text('Select network: '),
+              ),
+              Expanded(
+                child: DropdownButton(
+                  value: _networkName,
+                  items: _networks
+                      .map(
+                        (value) => DropdownMenuItem(
+                          value: value,
+                          child: Text(value),
+                        ),
+                      )
+                      .toList(),
+                  onChanged: _changeNetwork,
+                ),
+              ),
+            ],
+          ),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(
+                    left: 16,
+                    right: 16,
+                    bottom: 32,
+                  ),
+                  child: Text(
+                    'Connect to the $_networkName account through WalletConnect.',
+                    style: Theme.of(context).textTheme.bodyText1,
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: _transactionStateToAction(context, state: _state),
+                  child: Text(
+                    _transactionStateToString(state: _state),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  String _transactionStateToString({required ConnectionState state}) {
+    switch (state) {
+      case ConnectionState.disconnected:
+        return 'Connect!';
+      case ConnectionState.connecting:
+        return 'Connecting';
+      case ConnectionState.connected:
+        return 'Session connected';
+      case ConnectionState.connectionFailed:
+        return 'Connection failed';
+      case ConnectionState.connectionCancelled:
+        return 'Connection cancelled';
+    }
+  }
+
+  void _openWalletPage() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => WalletPage(connector: connector),
+      ),
+    );
+  }
+
+  VoidCallback? _transactionStateToAction(BuildContext context,
+      {required ConnectionState state}) {
+    print('State: ${_transactionStateToString(state: state)}');
+    switch (state) {
+      // Progress, action disabled
+      case ConnectionState.connecting:
+        return null;
+      case ConnectionState.connected:
+        // Open new page
+        return () => _openWalletPage();
+
+      // Initiate the connection
+      case ConnectionState.disconnected:
+      case ConnectionState.connectionCancelled:
+      case ConnectionState.connectionFailed:
+        return () async {
+          setState(() => _state = ConnectionState.connecting);
+          try {
+            final session = await connector.connect(context);
+            if (session != null) {
+              setState(() => _state = ConnectionState.connected);
+              Future.delayed(Duration.zero, () => _openWalletPage());
+            } else {
+              setState(() => _state = ConnectionState.connectionCancelled);
+            }
+          } catch (e) {
+            print('WC exception occured: $e');
+            setState(() => _state = ConnectionState.connectionFailed);
+          }
+        };
+    }
+  }
+
+  void _changeNetwork(String? network) {
+    if (network == null || _networkName == network) return;
+
+    final index = _networks.indexOf(network);
+    // update connector
+    switch (index) {
+      case 0:
+        connector = EthereumConnector();
+        break;
+      case 1:
+        connector = AlgorandConnector();
+        break;
+    }
+
+    setState(
+      () {
+        _networkName = network;
+        _state = ConnectionState.disconnected;
+      },
+    );
+  }
+}
